524ed2d447eddaa03527c9fcf26fadfe2c53df9f
PragmaOnce <pcumpian1@gmail.com>
2020-06-06 22:12:17 +0200

    mutex to the rescue!

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"sync"
 )
 
+type msgMap struct {
+	values map[int]string
+
+	sync.Mutex
+}
+
 func main() {
 	var wg sync.WaitGroup
 
-	msgMap := make(map[int]string)
+	msgsIndexed := new(msgMap)
+
+	msgsIndexed.values = make(map[int]string)
 
 	messages := []string{
 		"Hello, GDG Marbella!",
@@ -19,16 +27,18 @@ func main() {
 	for i, msg := range messages {
 		wg.Add(1)
 
-		go func() {
-			msgMap[i] = msg
+		go func(msg string, index int) {
+			defer wg.Done()
 
-			wg.Done()
-		}()
+			msgsIndexed.Lock()
+			msgsIndexed.values[index] = msg
+			msgsIndexed.Unlock()
+		}(msg, i)
 	}
 
 	wg.Wait()
 
 	for i := range messages {
-		fmt.Println(msgMap[i])
+		fmt.Println(msgsIndexed.values[i])
 	}
 }
