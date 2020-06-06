7d1a8294a0727bc90b51462df8860d8f179b1f1c
PragmaOnce <pcumpian1@gmail.com>
2020-06-06 14:56:38 +0200

    race condition fixed

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
 	go func() {
 		fmt.Println("Hello, GDG Marbella!")
+		wg.Done()
 	}()
+
+	wg.Wait()
 }
