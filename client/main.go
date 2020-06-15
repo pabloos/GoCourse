3a7275341cae312940719ae128560cdb4b921d5d
PragmaOnce <pcumpian1@gmail.com>
2020-06-15 18:52:55 +0200

    jsonPOSTClient

--- client/main.go
+++ client/main.go
@@ -1,30 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 )
 
 const (
 	URL = "http://localhost:8080"
 )
 
+type greet struct {
+	Name     string `json:"name,omitempty"`
+	Location string `json:"location,omitempty"`
+}
+
 func main() {
 	client := &http.Client{}
 
-	resp, err := client.Get(URL)
+	greetReq := greet{
+		Name:     "John Doe",
+		Location: "USA",
+	}
+
+	buf := &bytes.Buffer{} // b := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(greetReq)
+
+	greetURL := fmt.Sprintf("%s/%s", URL, "greet")
+
+	resp, err := client.Post(greetURL, "application/json; charset=utf-8", buf)
 	if err != nil {
-		fmt.Print(err)
-		return
+		fmt.Println(err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(err)
-		return
-	}
-
-	fmt.Print(string(body))
+	io.Copy(os.Stdout, resp.Body)
 }
