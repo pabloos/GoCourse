8bb211a6ddaf8a4e0048e786d21dcb899fc304a2
PragmaOnce <pcumpian1@gmail.com>
2020-06-15 18:53:30 +0200

    jsonPOSTServer

--- server/main.go
+++ server/main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type greet struct {
+	Name     string `json:"name,omitempty"`
+	Location string `json:"location,omitempty"`
+}
 
 func main() {
 	httpServer := &http.Server{
 		Addr: ":8080",
 	}
 
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/greet", greetHandler)
 
 	if err := httpServer.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!\n"))
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	var t greet
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	greet := fmt.Sprintf("Hello %s, from %s\n", t.Name, t.Location)
+
+	w.Write([]byte(greet))
 }
